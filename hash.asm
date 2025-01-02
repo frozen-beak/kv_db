35070ae00ed9ad3929b298db94d8c637b48eff14
frozen-beak <77727680+frozen-beak@users.noreply.github.com>
2025-01-02 07:44:42 +0000

    impl of hash function using djb2 algo

--- hash.asm
+++ hash.asm
@@ -1,17 +1,43 @@
 global _start
 
 section .data
-    hello db "Hello, World"
-    len equ $ - hello
+    hello db "Hello, World", 0
+
+section .bss
+    hash resq 1                 ; Reserve space for hash (1 quadword, 8 bytes)
 
 section .text
 _start:
+    ; Init hash to 5381
+    mov rax, 5381
+    mov [hash], rax
+
+    ; Load addr of input
+    mov rsi, hello
+
+hash_loop:
+    ; Load the next byte of the input
+    movzx rax, byte [rsi]       ; Zero-extend the byte to 64-bit
+    cmp al, 0                   ; Check if we reached the null terminator w/ lower 8 bits of rax
+    je print_hash               ; Exit the loop at the end
+
+    ; Update hash, hash = hash * 33 + char
+    mov rbx, [hash]
+    imul rbx, rbx, 33
+    add rbx, rax
+    mov [hash], rbx
+
+    ; Advance to the next character
+    inc rsi
+    jmp hash_loop
+
+print_hash:
     mov rax, 1
     mov rdi, 1
-    mov rsi, hello
-    mov rdx, len
+    mov rsi, hash
+    mov rdx, 8
     syscall
 
     mov rax, 60
-    xor rdi, rdi        ; XOR with itself is 0, Compact way to exit(0)
+    xor rdi, rdi
     syscall
