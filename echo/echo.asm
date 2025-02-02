e7ceb20c11f1afd7d8e4a6ce45e96ee190c77704
adii <77727680+frozen-beak@users.noreply.github.com>
2025-02-02 15:44:04 +0530

    defined constants

--- echo/echo.asm
+++ echo/echo.asm
@@ -5,7 +5,23 @@
 
 global _start
 
+%define SYS_READ 0
+%define SYS_WRITE 1
+%define SYS_CLOSE 3
+%define SYS_POLL 7
+%define SYS_SOCKET 41
+%define SYS_ACCEPT 43
+%define SYS_BIND 49
+%define SYS_LISTEN 50
 %define SYS_EXIT 60
+%define SYS_FCNTL 72
+
+%define MAX_CLIENTS 1024
+%define POLLIN 0x0001
+%define POLLOUT 0x0004
+%define POLLERR 0x0008
+%define F_SETFL 3
+%define O_NONBLOCK 0x4000
 
 section .text
 _start:
