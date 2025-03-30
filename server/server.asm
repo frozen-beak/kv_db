ad83a8b1386a33d8747d7a01577ce9ee279a0c61
adii <77727680+frozen-beak@users.noreply.github.com>
2025-03-30 19:10:12 +0000

    server socket

--- server/server.asm
+++ server/server.asm
@@ -1,7 +1,32 @@
 global _start
 
+%define SYS_SOCKET 41
+
+section .bss
+  server_fd resq 1              ; server socket fd
+
 section .text
 _start:
+  ;; create a listening socket
+  mov rax, SYS_SOCKET
+  mov rdi, 2
+  mov rsi, 1
+  xor rdx, rdx
+  syscall
+
+  test rax, rax
+  js error_exit
+
+  mov [server_fd], rax
+
+  jmp shutdown
+
+error_exit:
   mov rax, 60
-  mov rdi, 0
+  mov rdi, 1
+  syscall
+
+shutdown:
+  mov rax, 60
+  xor rdi, rdi
   syscall
