0d9141d7c04369d728ede9a93f7977ae0959d234
adii <77727680+frozen-beak@users.noreply.github.com>
2025-04-06 19:18:08 +0000

    basic I/O

--- ll/v0/ll.asm
+++ ll/v0/ll.asm
@@ -1,7 +1,24 @@
 global _start
 
+section .data
+  buffer resb 16
+
 section .text
 _start:
-  mov rax, 60
-  mov rdi, 0
+  ;; read from stdin
+  mov rax, 0x00
+  mov rdi, 0x00
+  lea rsi, [buffer]
+  mov rdx, 128
+  syscall
+
+  ;; write to stdout
+  mov rdx, rax
+  mov rax, 0x01
+  mov rdi, 0x01
+  lea rsi, [buffer]
+  syscall
+
+  mov rax, 0x3C
+  mov rdi, 0x00
   syscall
